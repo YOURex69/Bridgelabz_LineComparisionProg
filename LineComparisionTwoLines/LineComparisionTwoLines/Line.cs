021ee12fd0f7416c1599f544a2e9e11eabfc8984
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-10 14:31:29 +0530

    Add line comparision 2 lines

---
+++ LineComparisionTwoLines/LineComparisionTwoLines/Line.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisionTwoLines
+{
+    public class Line
+    {
+
+        public double computelength(int x1, int y1, int x2, int y2)
+        {
+
+            int xabs = Math.Abs(x2 - x1);
+            int yabs = Math.Abs(y2 - y1);
+
+            int distance_square = (int)(Math.Pow(xabs, 2) + Math.Pow(yabs, 2));
+
+            double length = Math.Sqrt(distance_square);
+            return length;
+        }
+
+
+    }
+}
